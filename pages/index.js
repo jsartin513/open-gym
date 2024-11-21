92565041c73009e49d346946b9252fb3b19fb7ec
Jessica Sartin <jessica.sartin@enolink.com>
2024-11-21 12:58:21 -0500

    Make team display cleaner

--- pages/index.js
+++ pages/index.js
@@ -212,16 +212,18 @@ export default function Home() {
         <div>
           <h2>Teams</h2>
           <button onClick={clearTeams} className={styles.clearButton}>Clear Teams</button>
-          {teams.map((team, index) => (
-            <div key={index}>
-              <h3>Team {index + 1}</h3>
-              <ul>
-                {team.map(player => (
-                  <li key={player.name}>{player.name}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
+          <div className={styles.teamOuter}>
+            {teams.map((team, index) => (
+              <div key={index}>
+                <h3>Team {index + 1}</h3>
+                <ul>
+                  {team.map(player => (
+                    <li key={player.name}>{player.name}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
 
