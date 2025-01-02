63a4c1eab70ab05d602b18233762604e0caa316e
Jess Sartin <jdoherty513@gmail.com>
2025-01-02 11:42:06 -0500

    Jls/more stuff like timer (#29)
    Timer updates

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import styles from "../styles/GamesPage.module.css";
 
 const Timer = ({ mode }) => {
-  const TIMER_OFFSET = 1; // seconds before the actual time to speak
+  const TIMER_OFFSET = 2; // seconds before the actual time to speak
+  const SECONDS_TO_SPEAK = [60, 30, 20, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
   const [gameTimer, setGameTimer] = useState(180);
   const [gameLength, setGameLength] = useState(180);
@@ -18,23 +19,27 @@ const Timer = ({ mode }) => {
     if (mode === "cloth") {
       return "Game over";
     } else {
-      return "Reset blocking";
+      return "Reset for no-blocking";
     }
   }
 
   function speakTimeIfRelevant(timerSeconds) {
-    const seconds = timerSeconds - TIMER_OFFSET;
-    if (seconds === 0) {
-      speak(endGameText());
-    } else if (seconds === 60) {
-      speak("One minute left");
-    } else if (seconds % 60 === 0) {
-      speak(`${seconds / 60} minutes left`);
-    } else if ([30, 20, 10].includes(seconds)) {
-      speak(`${seconds} seconds left`);
-    } else if (isTimerRunning && seconds > 0 && seconds < 10) {
-      speak(seconds);
+    const seconds = timerSeconds + TIMER_OFFSET;
+    if (SECONDS_TO_SPEAK.includes(timerSeconds)) {
+      speak(timerSeconds);
     }
+    else if (SECONDS_TO_SPEAK.includes(seconds) && seconds > 10) {
+      speak("seconds");
+      if (mode === "cloth") {
+        speak("left");
+      } else {
+        speak("until no-block");
+      }
+    }
+    else if (timerSeconds === 0) {
+      speak("BEEEEEEEEEEP" + endGameText());
+    }
+    
   }
 
   const toggleGameDuration = () => {
@@ -93,6 +98,14 @@ const Timer = ({ mode }) => {
     setGameTimer(gameLength);
   };
 
+  const fontSize = () => {
+    if (isTimerRunning || preGameCountdown !== null) {
+      return "text-8xl";
+      } else {
+      return "text-4xl";
+    }
+  }
+
   return (
     <div className="p-4 bg-white rounded-lg shadow-md"> 
       <h2 className="text-xl font-bold mb-4">Game Timer</h2> 
@@ -106,9 +119,9 @@ const Timer = ({ mode }) => {
       )}
 
       {preGameCountdown !== null ? (
-        <p>Starting in {preGameCountdown}...</p>
+        <p className="text-4xl">Starting in {preGameCountdown}...</p>
       ) : (
-        <p className={`text-lg ${gameTimer <= 10 ? 'text-red-500 animate-pulse' : ''}`}> {/* Conditional styling */}
+        <p className={`${fontSize()} ${gameTimer <= 10 ? 'text-red-500 animate-pulse' : ''}`}> {/* Conditional styling */}
           {formatTime(gameTimer)}{" "}
           {gameTimer <= 0 && (mode === "cloth" ? "Game Over!" : "No Blocking!")}
         </p>
